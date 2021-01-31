d3ce9ff72afe8d9df9266ce42b888c8209380bbc
qwertypnk <qwertypnk@protonmail.com>
2021-01-31 08:30:55 +0200

    chore: update

--- sotaoi/omni/scripts/dev/setup-env.ts
+++ sotaoi/omni/scripts/dev/setup-env.ts
@@ -3,22 +3,22 @@ import Os from 'os';
 import fs from 'fs';
 import path from 'path';
 import yargs from 'yargs';
+import { AppInfo } from '@sotaoi/omni/state';
+
+const argv = yargs
+  .option('info', {
+    alias: 'i',
+    description: 'File path for app info.json',
+    type: 'string',
+  })
+  .help()
+  .alias('help', 'h').argv;
+if (!argv.info) {
+  throw new Error('File path for app info.json is missing. --info is required');
+}
+const info: AppInfo = JSON.parse(fs.readFileSync(path.resolve(argv.info)).toString());
 
 const main = (func: () => any): any => {
-  const argv = yargs
-    .option('info', {
-      alias: 'i',
-      description: 'File path for app info.json',
-      type: 'string',
-    })
-    .help()
-    .alias('help', 'h').argv;
-
-  if (!argv.info) {
-    throw new Error('File path for app info.json is missing. --info is required');
-  }
-  const info = JSON.parse(fs.readFileSync(path.resolve(argv.info)).toString());
-
   // create snakeoil certs
   execSync(`./var/create-certs.sh ${info.devDomain}`, { stdio: 'inherit' });
   console.debug('\nadding certificates to trust store...');
@@ -48,16 +48,38 @@ switch (Os.platform()) {
     break;
   case 'linux':
     main(() => {
-      execSync('sudo cp ./sotaoi/api/certs/fullchain.pem /usr/local/share/ca-certificates/fullchain.crt', {
-        stdio: 'inherit',
-      });
-
-      const redHatUpdateCaTrust = '/bin/update-ca-trust';
       switch (true) {
-        case fs.existsSync(redHatUpdateCaTrust):
-          execSync(redHatUpdateCaTrust, { stdio: 'inherit' });
+        // centos / redhat
+        case fs.existsSync('/bin/update-ca-trust'):
+          if (fs.existsSync(`sudo /usr/local/share/ca-certificates/${info.packageName}`)) {
+            execSync('/bin/update-ca-trust', { stdio: 'inherit' });
+            console.warn('warning: trust store has certificates folder, skipping certificate generation');
+            return;
+          }
+          execSync(`sudo mkdir -p /usr/local/share/ca-certificates/${info.packageName}`, { stdio: 'inherit' });
+          execSync(
+            `sudo cp ./sotaoi/api/certs/fullchain.pem /usr/local/share/ca-certificates/${info.packageName}/fullchain.crt`,
+            {
+              stdio: 'inherit',
+            },
+          );
+          execSync('/bin/update-ca-trust', { stdio: 'inherit' });
           break;
+        // debian
         default:
+          if (fs.existsSync(`sudo /usr/local/share/ca-certificates/${info.packageName}`)) {
+            console.warn('warning: trust store has certificates folder, skipping certificate generation');
+            return;
+          }
+          execSync(`sudo mkdir -p /usr/local/share/ca-certificates/${info.packageName}`, { stdio: 'inherit' });
+          execSync(
+            `sudo cp ./sotaoi/api/certs/fullchain.pem /usr/local/share/ca-certificates/${info.packageName}/fullchain.crt`,
+            {
+              stdio: 'inherit',
+            },
+          );
+          // execSync('sudo dpkg-reconfigure -p critical ca-certificates');
+          execSync('sudo dpkg-reconfigure ca-certificates');
           execSync('sudo update-ca-certificates', { stdio: 'inherit' });
       }
     });

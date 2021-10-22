044da9e161680e77702d966d0cdc8cdabf134712
qwertypnk <qwertypnk@protonmail.com>
2021-10-22 10:59:36 +0300

    fix: smallfix

--- scripts/hook/hook-postinstall.js
+++ scripts/hook/hook-postinstall.js
@@ -4,9 +4,17 @@ const fs = require('fs');
 const path = require('path');
 
 const main = async () => {
+  //
+
   !fs.existsSync(path.resolve('./.env')) && fs.copyFileSync(path.resolve('./.env.example'), path.resolve('./.env'));
+
   !fs.existsSync(path.resolve('./pocket/env.json')) &&
     fs.copyFileSync(path.resolve('./pocket/env.example.json'), path.resolve('./pocket/env.json'));
+
+  !fs.existsSync(path.resolve('./scripts/service.js')) &&
+    fs.copyFileSync(path.resolve('./scripts/service.example.js'), path.resolve('./scripts/service.js'));
+
+  //
 };
 
 main();

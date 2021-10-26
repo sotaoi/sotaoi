124377244503d840c89c584add9afdb3e5d76f0c
qwertypnk <qwertypnk@protonmail.com>
2021-10-26 09:33:04 +0300

    refactor: update

--- scripts/stop/stop-servers-prod.js
+++ scripts/stop/stop-servers-prod.js
@@ -3,7 +3,23 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-execSync('npm run stop:proxy:prod', { cwd: path.resolve('./app-proxy'), stdio: 'inherit' });
-execSync('npm run stop:api:prod', { cwd: path.resolve('./app-api'), stdio: 'inherit' });
-execSync('npm run stop:auth:prod', { cwd: path.resolve('./app-auth'), stdio: 'inherit' });
-execSync('npm run stop:streaming:prod', { cwd: path.resolve('./app-streaming'), stdio: 'inherit' });
+try {
+  execSync('npm run stop:proxy:prod', { cwd: path.resolve('./app-proxy'), stdio: 'inherit' });
+} catch (err) {
+  console.error(err);
+}
+try {
+  execSync('npm run stop:api:prod', { cwd: path.resolve('./app-api'), stdio: 'inherit' });
+} catch (err) {
+  console.error(err);
+}
+try {
+  execSync('npm run stop:auth:prod', { cwd: path.resolve('./app-auth'), stdio: 'inherit' });
+} catch (err) {
+  console.error(err);
+}
+try {
+  execSync('npm run stop:streaming:prod', { cwd: path.resolve('./app-streaming'), stdio: 'inherit' });
+} catch (err) {
+  console.error(err);
+}

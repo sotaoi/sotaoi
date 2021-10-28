3b48a755282a3e3dce953021671f1294961ab843
qwertypnk <qwertypnk@protonmail.com>
2021-10-28 08:47:39 +0300

    refactor: update

--- shell/routines/setup-mac.sh
+++ shell/routines/setup-mac.sh
@@ -4,6 +4,10 @@
 
 echo "Running setup for Mac...";
 
+if [[ $(which brew) == "" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 if [[ $(which gls) == "" ]]; then
   brew install coreutils
 fi
